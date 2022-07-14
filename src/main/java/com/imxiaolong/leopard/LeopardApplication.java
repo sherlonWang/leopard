7e87fb55033bf1169e5e9ccdaf0e5ee5c97bd227
sherlonwang <15061460796@163.com>
2022-07-15 00:53:12 +0800

    init project

---
+++ src/main/java/com/imxiaolong/leopard/LeopardApplication.java
@@ -0,0 +1,13 @@
+package com.imxiaolong.leopard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeopardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeopardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imxiaolong/leopard/LeopardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imxiaolong.leopard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeopardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
